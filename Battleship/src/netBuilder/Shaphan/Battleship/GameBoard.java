5415a5955d4b2a45a0c53632d714221cf83ce983
Shaphan <shaphan.martin@gmail.com>
2014-12-10 14:57:37 +0000

    Prints grid to screen at any size also can show grid multiple times

--- Battleship/src/netBuilder/Shaphan/Battleship/GameBoard.java
+++ Battleship/src/netBuilder/Shaphan/Battleship/GameBoard.java
@@ -1,38 +1,65 @@
 package netBuilder.Shaphan.Battleship;
 
 public class GameBoard {
-	int rows = 11;
-	int columns = 11;
-	
-	int [][] grid = new int[rows][columns];
-	
-	
-	
-	public void clearGrid(){ 
-	for(int i = 0; i < grid.length; i++ )
-		for(int j = 0; i < grid.length; j++)
-			grid [i][j] =0;
-	}
-	
-	public void printGrid(){
-		
-		
-		for (int clbl = 0; clbl < grid.length; ++clbl){
-			grid [clbl][0] = clbl;
-		}
+	int gridSize = 20;
+	char character = 'A';
+
+	String[][] grid = new String[gridSize][gridSize];
+
+	public void initGrid() {
+		for (int column = 0; column < grid.length; column++) {
+			for (int row = 0; row < grid.length; row++) {
 			
-		for(int i = 0; i < grid.length; ++i) {
-		    for(int j = 0; j < grid[i].length; ++j) {
-		         System.out.printf("%5d ", grid[i][j]);
-		      }
-		      System.out.println();
-		      System.out.println();
+				grid[row][column] = "~";
+			}
+		}
 	}
+
+	public void printGrid() {
+		char character = 'A';
+
+		System.out.println();
+
+		// printing the row labels
+		for (int row = 0; row < grid.length; row++) {
+			grid[0][row] = String.valueOf(row);
+			System.out.print("  " + grid[0][row] + " \t");
+
+		}
+
+		// printing the columns labels
+
+		for (int column = 0; column < grid.length; column++) {
+			System.out.println();
+			System.out.println();
+			grid[1][column] = String.valueOf(character);
+			character++;
+			System.out.print(grid[1][column] + "\t");
+			for (int row = 0; row < grid.length; row++) {
+				//if (grid[row][column] == "~") {
+				//	System.out.print("  " + grid[row][column] + "\t");
+					
+			//	} 
+			if (grid[row][column] == "X") {
+					System.out.print("  " + grid[row][column] + "\t");
+
+				} else if (grid[row][column] == "*") {
+					System.out.print("  " + grid[row][column] + "\t");
+				}else{
+					grid[row][column] = "~";
+					System.out.print("  " + grid[row][column] + "\t");
+
+					
+				}
+
+			}
+
+		}
+
 	}
-	
-	
-	public void fire(int xCoord , int yCoord){
-		grid [xCoord][yCoord] = 2;
+
+	public void fire(int xCoord, int yCoord) {
+		grid[xCoord][yCoord] = "X";
 	}
-	
+
 }
